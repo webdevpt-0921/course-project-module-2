93a134fdff4508acbd83d2b51643c619c75d5120
zapatran <zapatran@users.noreply.github.com>
2021-12-08 18:54:40 +0100

    initial commit

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const setupApp = require("./app");
-const { startDB, stopDB } = require("./db");
+const setupApp = require('./app');
+const { startDB, stopDB } = require('./db');
 
 const port = process.env.PORT || 3000;
 
 function stopServer(server) {
-  server.close((error) => {
+  server.close(error => {
     if (error) {
       console.error(error.message);
     }
-    console.log("stopped http server");
+    console.log('stopped http server');
   });
 }
 
@@ -17,33 +17,31 @@ function run() {
   let httpServer;
 
   startDB()
-    .then((db) => {
-      console.log(
-        `Connected to Mongo! Database name: "${db.connections[0].name}"`
-      );
+    .then(db => {
+      console.log(`Connected to Mongo! Database name: "${db.connections[0].name}"`);
     })
     .then(() => {
       httpServer = app.listen(port, () => {
         console.log(`Starting on port ${port}`);
       });
     })
-    .catch((e) => {
-      console.log("error starting server", e);
+    .catch(e => {
+      console.log('error starting server', e);
     });
 
   function stop() {
-    console.log("stop");
+    console.log('stop');
     stopDB()
       .then(() => {
         stopServer(httpServer);
       })
-      .catch((e) => {
-        console.log("error closing db");
+      .catch(e => {
+        console.log('error closing db');
       });
   }
 
-  process.on("SIGINT", stop);
-  process.on("SIGTERM", stop);
+  process.on('SIGINT', stop);
+  process.on('SIGTERM', stop);
 }
 
 run();
